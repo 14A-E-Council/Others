e7bb15867bfcf59e950a7c86981d5f4cff388431
BlascsokTibor <113507766+BlascsokTibor@users.noreply.github.com>
2023-04-28 11:55:11 +0200

    Update AdatbázisLétrehozása.sql

--- Adatbázis/AdatbázisLétrehozása.sql
+++ Adatbázis/AdatbázisLétrehozása.sql
@@ -2,27 +2,139 @@ CREATE DATABASE IF NOT EXISTS brakingpoint
 	CHARACTER SET utf8
 	COLLATE utf8_hungarian_ci;
 
+CREATE TABLE brakingpoint.bans (
+  id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  bannable_type VARCHAR(255) NOT NULL,
+  bannable_id BIGINT(20) UNSIGNED NOT NULL,
+  created_by_type VARCHAR(255) DEFAULT NULL,
+  created_by_id BIGINT(20) UNSIGNED DEFAULT NULL,
+  comment TEXT DEFAULT NULL,
+  expired_at TIMESTAMP NULL DEFAULT NULL,
+  deleted_at TIMESTAMP NULL DEFAULT NULL,
+  created_at TIMESTAMP NULL DEFAULT NULL,
+  updated_at TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS brakingpoint.users (
-  userID INT(11) NOT NULL AUTO_INCREMENT,
+ALTER TABLE brakingpoint.bans 
+  ADD INDEX bans_bannable_type_bannable_id_index(bannable_type, bannable_id);
+
+ALTER TABLE brakingpoint.bans 
+  ADD INDEX bans_created_by_type_created_by_id_index(created_by_type, created_by_id);
+
+ALTER TABLE brakingpoint.bans 
+  ADD INDEX bans_expired_at_index(expired_at);
+
+
+
+
+CREATE TABLE brakingpoint.users (
+  userID INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   username VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) DEFAULT NULL,
   first_name VARCHAR(255) DEFAULT NULL,
   email VARCHAR(50) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  balance INT(11) NOT NULL,
-  registration_date DATE NOT NULL,
+  two_factor_secret TEXT DEFAULT NULL,
+  two_factor_recovery_codes TEXT DEFAULT NULL,
+  two_factor_confirmed_at TIMESTAMP NULL DEFAULT NULL,
+  balance INT(11) NOT NULL DEFAULT 0,
   preferred_category VARCHAR(255) DEFAULT NULL,
-  level INT(11) NOT NULL,
+  level INT(11) NOT NULL DEFAULT 1,
   picture_frame VARCHAR(255) DEFAULT NULL,
-  rank INT(11) NOT NULL,
+  rank INT(11) NOT NULL DEFAULT 0,
   colour_palette VARCHAR(255) DEFAULT NULL,
-  profile_picture VARCHAR(255) DEFAULT NULL,
+  updated_at DATETIME NOT NULL,
+  created_at DATETIME NOT NULL,
+  profile_picture VARCHAR(255) NOT NULL DEFAULT 'default.png',
+  xp INT(11) NOT NULL DEFAULT 0,
+  admin TINYINT(2) NOT NULL DEFAULT 0,
+  email_verified_at DATETIME DEFAULT NULL,
+  google_id VARCHAR(255) DEFAULT NULL,
+  facebook_id VARCHAR(255) DEFAULT NULL,
+  remember_token VARCHAR(255) DEFAULT NULL,
+  banned_at DATETIME DEFAULT NULL,
   PRIMARY KEY (userID)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 30,
+AVG_ROW_LENGTH = 8192,
+CHARACTER SET utf8,
+COLLATE utf8_hungarian_ci;
+
+
+CREATE TABLE brakingpoint.sessions (
+  id VARCHAR(255) NOT NULL,
+  user_id BIGINT(20) UNSIGNED DEFAULT NULL,
+  ip_address VARCHAR(45) DEFAULT NULL,
+  user_agent TEXT DEFAULT NULL,
+  payload LONGTEXT NOT NULL,
+  last_activity INT(11) NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 5461,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE brakingpoint.sessions 
+  ADD INDEX sessions_user_id_index(user_id);
+
+ALTER TABLE brakingpoint.sessions 
+  ADD INDEX sessions_last_activity_index(last_activity);
+
+
+CREATE TABLE brakingpoint.personal_access_tokens (
+  id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  tokenable_type VARCHAR(255) NOT NULL,
+  tokenable_id BIGINT(20) UNSIGNED NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  token VARCHAR(64) NOT NULL,
+  abilities TEXT DEFAULT NULL,
+  last_used_at TIMESTAMP NULL DEFAULT NULL,
+  expires_at TIMESTAMP NULL DEFAULT NULL,
+  created_at TIMESTAMP NULL DEFAULT NULL,
+  updated_at TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE brakingpoint.personal_access_tokens 
+  ADD UNIQUE INDEX personal_access_tokens_token_unique(token);
+
+ALTER TABLE brakingpoint.personal_access_tokens 
+  ADD INDEX personal_access_tokens_tokenable_type_tokenable_id_index(tokenable_type, tokenable_id);
+
+
+CREATE TABLE brakingpoint.password_resets (
+  email VARCHAR(50) DEFAULT NULL,
+  token VARCHAR(255) DEFAULT NULL,
+  created_at DATETIME DEFAULT NULL
 )
 ENGINE = INNODB,
 CHARACTER SET utf8,
 COLLATE utf8_hungarian_ci;
+
+ALTER TABLE brakingpoint.password_resets 
+  ADD UNIQUE INDEX email(email);
+
+
+CREATE TABLE brakingpoint.migrations (
+  id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  migration VARCHAR(255) NOT NULL,
+  batch INT(11) NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 6,
+AVG_ROW_LENGTH = 4096,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_unicode_ci;
 
 
 CREATE TABLE IF NOT EXISTS brakingpoint.sports (
@@ -111,20 +223,7 @@ CREATE TABLE IF NOT EXISTS brakingpoint.tickets (
 )
 ENGINE = INNODB,
 CHARACTER SET utf8,
-COLLATE utf8_hungarian_ci;
-    
-
-CREATE TABLE IF NOT EXISTS brakingpoint.connection_user_sport (
-  connectionID INT(11) NOT NULL AUTO_INCREMENT,
-  sportID INT(11) NOT NULL,
-  userID INT(11) NOT NULL,
-  PRIMARY KEY (connectionID),
-  FOREIGN KEY (sportID) REFERENCES brakingpoint.sports(sportID),
-  FOREIGN KEY (userID) REFERENCES brakingpoint.users(userID)
-)
-ENGINE = INNODB,
-CHARACTER SET utf8,
-COLLATE utf8_hungarian_ci;
+COLLATE utf8_hungarian_ci;    
 
 
 CREATE TABLE IF NOT EXISTS brakingpoint.connection_available_bet_ticket (
